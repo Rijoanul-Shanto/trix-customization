a49d5b1c36fa0277fa4700ed4a4acf9ed48a8efb
Rijoanul-Shanto <rijoanul.shanto@gmail.com>
2021-01-25 19:18:32 +0600

    [ADD] - Slash reset functionality added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let init = (function () {
+let trixInit = (function () {
     let element = document.querySelector("trix-editor");
     let slashTriggered = false;
     let searchKeyStartPosition = -1;
@@ -10,35 +10,54 @@ let init = (function () {
     }
 }());
 
-let searchKey = function () {
-    let documentTextElement = init.element.innerText;
-    let caretPosition = init.element.editor.getSelectedRange()[0];
 
-    keyString = documentTextElement.slice(init.searchKeyStartPosition + 1, caretPosition);
+let searchKey = function () {
+    let documentTextElement = trixInit.element.innerText;
+    let caretPosition = trixInit.element.editor.getSelectedRange()[0];
+
+    keyString = documentTextElement.slice(trixInit.searchKeyStartPosition + 1, caretPosition);
 
     console.log('~~~', keyString);
 }
 
 let isSlashAllowed = function () {
-    let documentTextElement = init.element.innerText;
-    let slashPosition = init.element.editor.getSelectedRange()[0] - 1;
+    let documentTextElement = trixInit.element.innerText;
+    let slashPosition = trixInit.element.editor.getSelectedRange()[0] - 1;
 
     return (0 === slashPosition || '\n' === documentTextElement[slashPosition - 1]) ? slashPosition : -1;
+}
+
+let slashReset = function () {
+    if (trixInit.slashTriggered) {
+        trixInit.element.editor.setSelectedRange([
+            trixInit.searchKeyStartPosition,
+            trixInit.element.editor.getSelectedRange()[0]
+        ]);
+
+        trixInit.element.editor.deleteInDirection("forward");
+
+        trixInit.searchKeyStartPosition = -1;
+        trixInit.slashTriggered = false;
+    }
 }
 
 let handleKeyPress = function (pressedKey) {
     console.log(pressedKey);
     if ('/' === pressedKey) {
         let result = isSlashAllowed();
-        -1 !== result ? (init.searchKeyStartPosition = result, init.slashTriggered = true) : undefined;
+        -1 !== result ? (trixInit.searchKeyStartPosition = result, trixInit.slashTriggered = true) : undefined;
     }
     else if ('Enter' === pressedKey) {
-        init.slashTriggered ? (init.searchKeyStartPosition = -1, init.slashTriggered = false) : undefined;
-    } else {
-        init.slashTriggered ? searchKey() : undefined;
+        slashReset();
+    }
+    else if (' ' === pressedKey) {
+        slashReset();
+    }
+    else {
+        trixInit.slashTriggered ? searchKey() : undefined;
     }
 }
 
-init.element.addEventListener("keyup", function (event) {
+trixInit.element.addEventListener("keyup", function (event) {
     handleKeyPress(event.key);
 })
